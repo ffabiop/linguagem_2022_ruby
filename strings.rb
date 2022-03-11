6af0b7be1b1ffed66cc3269707c41ac70f38f45b
ffabiop <ffabiop@gmail.com>
2022-03-10 22:32:48 -0300

    strings pratica

---
+++ strings.rb
@@ -0,0 +1,30 @@
+nome = "Fabio"
+
+puts ["A","B","C"]
+
+print ["A","B","C"]
+
+p ["A","B","C"]
+
+p nome
+
+p nome[0]
+
+p nome[3]
+
+p nome[nome.length - 1]
+
+p nome[-1]
+
+p nome[-2]
+
+p nome.chars
+
+p nome[0,3]
+
+puts "cabeçalho"
+puts "-"*10
+puts "testo escrito aqui"
+
+numero = 42
+puts "o numero é %05d" % numero
