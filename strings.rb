1db2e50d1570e6d46e0b7331ebf8f73377798621
ffabiop <ffabiop@gmail.com>
2022-03-11 22:17:10 -0300

    strings operacoes

--- strings.rb
+++ strings.rb
@@ -28,3 +28,29 @@ puts "testo escrito aqui"
 
 numero = 42
 puts "o numero é %05d" % numero
+
+mensagem = "       teste de mensagem    "
+puts mensagem
+puts mensagem.strip
+
+mensagem = "fabio"
+
+puts mensagem.upcase
+mesnagem = mensagem.upcase
+puts mensagem.downcase
+
+puts mensagem.upcase
+mensagem.upcase! #muda conteudo da variavel
+puts mensagem.downcase
+
+mensagem = "minha mensagem"
+puts mensagem.capitalize
+
+mensagem = "olá nome"
+puts mensagem
+puts mensagem.gsub("nome", "fabio")
+
+nomes = "fabio roberto pelicer".split
+puts nomes
+
+puts "fabio-roberto-pelicer".split("-")
